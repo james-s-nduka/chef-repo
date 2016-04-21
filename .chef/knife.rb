5909c4fb77dc40b493647f3e89c06c72558173fa
Admin <james.s.nduka@synwin.co.uk>
2016-04-21 10:54:14 +0100

    Update DEV env

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,12 +1,9 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
-
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "james_nduka"
-client_key               "#{current_dir}/james_nduka.pem"
-validation_client_name   "cd-demo-validator"
-validation_key           "#{current_dir}/cd-demo-validator.pem"
-chef_server_url          "https://52.48.100.217/organizations/cd-demo"
-cookbook_path            ["#{current_dir}/../cookbooks"]
-ssl_verify_mode          :verify_none
+node_name                "administrator"
+client_key               "#{current_dir}/administrator.pem"
+validation_client_name   "synwin-cloud-validator"
+validation_key           "#{current_dir}/synwin-cloud-validator.pem"
+chef_server_url          "https://chef.syndicate-devops.net/organizations/synwin-cloud"
+cookbook_path            ["#{current_dir}/../chef-repo/cookbooks"]
